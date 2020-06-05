ebb61563b1e566f938aa6de3379768307f8018af
Jansen Li <jansenli89@gmail.com>
2020-06-05 18:00:55 -0400

    sync

--- keyboards/massdrop/alt/keymaps/custom/keymap.c
+++ keyboards/massdrop/alt/keymaps/custom/keymap.c
@@ -18,14 +18,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP, \
         LT(2, KC_GRAVE), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGDN, \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_END, \
-        KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             MO(1), MO(1),   KC_LEFT, KC_DOWN, KC_RGHT  \
+        KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             MO(1), MO(3),   KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
     [1] = LAYOUT_65_ansi_blocker(
-        _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, LALT(KC_F4), \
-        _______, RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, _______, U_T_AUTO,U_T_AGCR,_______, KC_PSCR, KC_SLCK, KC_PAUS, _______, LCTL(LALT(KC_DEL)), \
-        _______, RGB_RMOD,RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, KC_PGUP,          _______, _______, \
-        _______, _______, RGB_TOG, _______, _______, DBG_TOG, NK_TOGG, MD_BOOT, _______, _______, KC_PGDN, _______,          _______, _______, \
-        _______, _______, _______,                            _______,                            KC_RALT, _______, KC_RCTL, _______, _______  \
+        _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, \
+        _______, _______, _______, _______, _______, _______, _______, _______,_______,_______, _______, _______, _______, _______, _______, \
+        _______, LSFT(KC_LBRC), LSFT(KC_RBRC), _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, \
+        _______, KC_LBRC, KC_RBRC, _______, _______, _______, _______, KC_MINS, KC_EQL, LSFT(KC_9), LSFT(KC_0), _______,          _______, _______, \
+        _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______  \
     ),
     [2] = LAYOUT_65_ansi_blocker(
         KC_NLCK,  KC_P1,   KC_P2,   KC_P3,   KC_P4,   KC_P5,   KC_P6,   KC_P7,   KC_P8,   KC_P9,   KC_P0,  _______,  _______, _______, _______, \
@@ -33,6 +33,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, KC_VOLD, KC_VOLU, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, _______,          _______, _______, \
         _______, KC_MUTE, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, \
         _______, _______, _______,                            _______,                             	_______, _______, _______, _______, _______  \
+    ),
+    [3] = LAYOUT_65_ansi_blocker(
+        _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, LALT(KC_F4), \
+        _______, RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, U_T_AUTO, U_T_AGCR, NK_TOGG, DBG_TOG, KC_PSCR, KC_SLCK, KC_PAUS, _______, LCTL(LALT(KC_DEL)), \
+        _______, RGB_RMOD,RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, KC_PGUP,          _______, _______, \
+        _______, _______, RGB_TOG, _______, _______, MD_BOOT, _______, _______, _______, _______, KC_PGDN, _______,          _______, _______, \
+        _______, _______, _______,                            _______,                            KC_RALT, _______, KC_RCTL, _______, _______  \
     ),
 };
 
