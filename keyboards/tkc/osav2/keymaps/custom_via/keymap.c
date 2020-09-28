e55789116075d3e2b1421831faf784d9669ded8a
Jansen Li <jansenli89@gmail.com>
2020-09-27 21:25:23 -0400

    added layer 3 and change space to layer tap

--- keyboards/tkc/osav2/keymaps/custom_via/keymap.c
+++ keyboards/tkc/osav2/keymaps/custom_via/keymap.c
@@ -21,21 +21,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_PGUP, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
   KC_PGDN, LT(2, KC_GRAVE), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
            KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,	                     KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_END,
-           KC_LCTL,          KC_LGUI,          KC_SPC,  KC_LALT,                     KC_SPC,           LT(1, KC_BSLASH),                            KC_RCTL
+           KC_LCTL,          KC_LGUI,          KC_SPC,  KC_LALT,                     LT(3, KC_SPC),           LT(1, KC_BSLASH),                            KC_RCTL
   ),
 [1] = LAYOUT_all(
   KC_TRNS, KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,            KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_TRNS,
   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI,          RGB_VAD, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS,          KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_TRNS, KC_PGUP,          KC_TRNS,
-           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET,                   RESET, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGDN, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_PGUP, KC_TRNS,          KC_TRNS,
+           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET,                   RESET, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGDN, KC_TRNS, KC_TRNS, KC_TRNS,
            KC_TRNS,          KC_TRNS,          KC_TRNS, KC_TRNS,                   KC_TRNS,                   KC_TRNS,                            KC_TRNS
   ),
 
 [2] = LAYOUT_all(
-  LALT(KC_F4), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  LALT(KC_F4), KC_TRNS, KC_P1,   KC_P2,   KC_P3,   KC_P4,   KC_P5,   KC_P6,          KC_P7,   KC_P8,   KC_P9,   KC_P0, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   LGUI(LSFT(KC_LEFT)), KC_MPLY, KC_MPRV, KC_MNXT, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   LGUI(LSFT(KC_RGHT)), KC_TRNS, KC_VOLD, KC_VOLU, KC_TRNS, KC_TRNS, KC_TRNS,         KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_TRNS, KC_TRNS,          KC_TRNS,
            KC_TRNS, KC_MUTE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+           KC_TRNS,          KC_TRNS,          KC_TRNS, KC_TRNS,                   KC_TRNS,                   KC_TRNS,                            KC_TRNS
+  ),
+[3] = LAYOUT_all(
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_PGUP, KC_TRNS,          KC_TRNS,
+           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGDN, KC_TRNS, KC_TRNS, KC_TRNS,
            KC_TRNS,          KC_TRNS,          KC_TRNS, KC_TRNS,                   KC_TRNS,                   KC_TRNS,                            KC_TRNS
   ),
 };
